a23cc6a4acce0f7abd3110972e4f7871eab65c16
puchal71 <puchal71@gmail.com>
2024-04-18 17:16:06 +0200

    Added category ordering. Possibility to change order order (frukter, gronsaker, etc.)

--- store/models.py
+++ store/models.py
@@ -4,10 +4,12 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
+    ordering = models.IntegerField(default=0)
 
     # Zmienia liczbe mnoga categoryes w panelu admina na Categories.
     class Meta:
         verbose_name_plural = 'Categories'
+        ordering = ('ordering',)
 
     def __str__(self):
         return self.title
